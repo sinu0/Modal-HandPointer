0387690d28a298ddba86116d4b151a5879bc05e4
Mariusz Pacek <pacekmariusz.email@gmail.com>
2013-04-09 23:26:27 +0200

    przykład sterowania myszka

--- Przyklad/main.cpp
+++ Przyklad/main.cpp
@@ -13,6 +13,10 @@
 #include <iostream>
 #include <stdio.h>
 
+#include <X11/Xlib.h> // Every Xlib program must include this
+#include <assert.h>   // I include this to test return values the lazy way
+#include <unistd.h>  
+
 using namespace std;
 using namespace cv;
 #include "opencv2/opencv.hpp"
@@ -26,6 +30,19 @@ int main(int, char**)
         return -1;
     Mat edges,frame;
     namedWindow("edges",1);
+    int x,y = 0;
+    const int maxY = 900;
+    const int maxX = 1600;
+    Display *dpy;
+    Window root_window;
+
+    dpy = XOpenDisplay(0);
+    root_window = XRootWindow(dpy, 0);
+    XSelectInput(dpy, root_window, KeyReleaseMask);
+
+    XWarpPointer(dpy, None, root_window, 0, 0, 0, 0, 100, 100);
+
+    XFlush(dpy);
     for(;;) {    
         cap >> frame; // gt a new frame from camera
         cvtColor(frame, edges, CV_BGR2GRAY);
@@ -34,6 +51,10 @@ int main(int, char**)
         Canny(edges, edges, 0,50, 3);
         flip(edges,edges,1);
         imshow("edges", edges);
+        XWarpPointer(dpy, None, root_window, 0, 0, 0, 0, x++, y++);
+        if (x > maxX) x=0;
+        if (y > maxY) y=0;
+        XFlush(dpy);
         if(waitKey(30) >= 0) break;
     }                    
     // the camera will be deinitialized automatically in VideoCapture destructor
